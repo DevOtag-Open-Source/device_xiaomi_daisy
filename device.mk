9d3899eb152147fa6f6456f7cbc22cbef51ca4a0
Ahmet Çelik <46200293+trkzmn@users.noreply.github.com>
2020-02-29 19:09:48 +0300

    daisy: Downgrade power hal version
    This iş patch-2
    
    Patch-1: https://github.com/Devotag-Open-Source/device_xiaomi_daisy/commit/00ad5e92c4a03ab0755a64803972f7833956c6d7

--- device.mk
+++ device.mk
@@ -343,7 +343,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
